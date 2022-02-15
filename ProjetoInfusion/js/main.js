51df513a236438a89ceb5a477a9d65985ac8933f
Mario Signorelli <signorelii2308@gmail.com>
2022-02-15 15:26:17 -0300

    fist commit

--- ProjetoInfusion/js/main.js
+++ ProjetoInfusion/js/main.js
@@ -3,4 +3,7 @@ let $body = document.querySelector('body');
     $body.classList('no-js');
     $body.classList('js');
 
+    var $btnmenu = document.querySelector('.header-btnmenu');
+    $btnmenu.removeAttribute('style');
+
 })()
